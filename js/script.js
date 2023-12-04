9db81b68af3ffcc688e5d443c601abddc45fd869
Aabir <aabirkashif@gmail.com>
2023-12-04 21:59:01 +0600

    created 16x16 grids

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+
+// grids
+
+const grid = document.querySelector(".container");
+
+//16x16 grid
+function grids () {
+    let number = 16;
+
+    for(let i = 1; i <= number; i++) {
+        const divs = document.createElement("div");
+        grid.appendChild(divs);
+    }
+}
+
+grids();
