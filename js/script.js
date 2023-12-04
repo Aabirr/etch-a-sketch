ec3db5dcd244c0be63b7f3aa1f89c49b94e8d244
Aabir <aabirkashif@gmail.com>
2023-12-04 23:16:29 +0600

    Game Reset button added

--- js/script.js
+++ js/script.js
@@ -7,11 +7,31 @@ const grid = document.querySelector(".container");
 //16x16 grid
 function grids () {
     let number = 16;
+    number *= 16;
 
     for(let i = 1; i <= number; i++) {
-        const divs = document.createElement("div");
-        grid.appendChild(divs);
-    }
-}
+        const childDivs = document.createElement("div");
 
+        childDivs.addEventListener('mouseover', () => {
+            childDivs.style.backgroundColor = "red";
+        });
+        grid.appendChild(childDivs);
+    }
+
+    //document.querySelector("divs");
+
+   
+}
 grids();
+
+//game reset button
+const buttons = document.createElement("button");
+buttons.textContent = "Reset Game";
+const gDivs = document.querySelector(".container > div");
+
+buttons.addEventListener('click', (button) => {
+
+    
+    location.reload();
+})
+grid.appendChild(buttons);
